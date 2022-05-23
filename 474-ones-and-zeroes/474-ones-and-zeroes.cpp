d32b115b3cc4c84c70e5d38cd3c98682dec41497
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-23 12:30:30 +0530

    Time: 493 ms (34.77%), Space: 102.1 MB (20.29%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-  int dp[601][101][101];
+ 
 
-int f(vector<string> &v,int index,int m,int n)
+int f(vector<string> &v,int index,int m,int n,vector<vector<vector<int>>>&dp)
 {
     if(index >= v.size()) return 0;
     
@@ -12,16 +12,15 @@ int f(vector<string> &v,int index,int m,int n)
     int zero = v[index].size() - one;
     
     if(m >=zero && n>= one){
-        return dp[index][m][n] = max(1+f(v,index+1,m-zero,n-one),f(v,index+1,m,n));
+        return dp[index][m][n] = max(1+f(v,index+1,m-zero,n-one,dp),f(v,index+1,m,n,dp));
        }
     else{
-         return dp[index][m][n] =f(v,index+1,m,n);
+         return dp[index][m][n] =f(v,index+1,m,n,dp);
      }
 }
     
     int findMaxForm(vector<string>& strs, int m, int n) {
-        memset(dp,-1,sizeof(dp));
-   // vector<vector<vector<int> > > dp(601,vector<vector<int> >(101,vector <int>(101,-1)));  //dp
-    return f(strs,0,m,n);
+       vector<vector<vector<int>>>dp(strs.size(),vector<vector<int>>(m+1,vector<int>(n+1,-1)));
+    return f(strs,0,m,n,dp);
     }
 };
