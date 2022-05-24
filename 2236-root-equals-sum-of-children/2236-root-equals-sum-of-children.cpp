a8aa82c0bd6bc308ff020ef44b7bcd54eafaa36b
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-24 23:28:39 +0530

    Time: 12 ms (6.42%), Space: 12.6 MB (15.63%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -1,28 +1,34 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    int preorder(TreeNode* root, int &sum){
-        if(!root) return 0;
-        preorder(root->left,sum);
-        sum+=root->val;
-          preorder(root->right,sum);
-        
-        return sum;
+class Solution
+{
+    public:
+        int inOrder(TreeNode *root, int &sum)
+        {
+            if (!root) return 0;
+
+            inOrder(root->left, sum);
+            sum += root->val;
+           inOrder(root->right, sum);
+
+            return sum;
         }
-    bool checkTree(TreeNode* root) {
-        int sum=0;
-          int su= preorder(root, sum)-root->val;
-        if(su==root->val)return true;
+
+    bool checkTree(TreeNode *root)
+    {
+        int sum = 0;
+        sum = inOrder(root, sum) - root->val;
+
+        if (sum == root->val) return true;
         return false;
     }
 };
