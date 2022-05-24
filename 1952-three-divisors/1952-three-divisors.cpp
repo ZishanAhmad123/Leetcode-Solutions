46213897964518d26d87d09255e11d0e85a1f762
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-24 22:50:33 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (71.28%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     void FastIO() {
+ ios_base::sync_with_stdio(0);cin.tie(0);}
+    
+    bool isThree(int n) {    
+           FastIO();
+         int cnt=1;
+      
+        for(int i=1;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                cnt++;
+            }
+        }
+        if(cnt==3)
+        {
+            return true;
+        }
+        return false;
+    }
+};
