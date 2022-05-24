0706f7edabb3e43acda545cec1f35c7064f73084
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-24 23:33:53 +0530

    Time: 0 ms (100.00%), Space: 12.4 MB (89.21%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -1,30 +1,34 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
+     void FastIO() {
+ ios_base::sync_with_stdio(0);cin.tie(0);}
+
         int inOrder(TreeNode *root, int &sum)
         {
             if (!root) return 0;
 
             inOrder(root->left, sum);
             sum += root->val;
-           inOrder(root->right, sum);
+            inOrder(root->right, sum);
 
             return sum;
         }
 
     bool checkTree(TreeNode *root)
     {
+        FastIO();
         int sum = 0;
         sum = inOrder(root, sum) - root->val;
 
