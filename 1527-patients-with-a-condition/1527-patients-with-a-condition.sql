68d4010faf5baea616a07a8f778d4a79300f1fa1
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-28 08:19:17 +0530

    Time: 424 ms (22.25%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
 
-SELECT *
-FROM Patients
-WHERE conditions LIKE 'DIAB1%'  #to check whether first condition is diab1
-	OR conditions LIKE '% DIAB1%'; #to check if any middle condition has diab1
+
+select patient_id, patient_name, conditions 
+  from  Patients
+    where conditions like  'DIAB1%' 
+         or
+          conditions like '% DIAB1%';
+     
