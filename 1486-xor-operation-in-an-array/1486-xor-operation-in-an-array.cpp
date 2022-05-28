2bae10f3098521bfeafad897e605f00461c985d2
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-28 18:35:25 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.73%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
-public:
+public:  void FastIO(){
+            ios_base::sync_with_stdio(0);cin.tie(0); }
+    
     int xorOperation(int n, int start) {
-      vector<int>v(n);
-      
+        FastIO();
+      int ans=0;
             for(int i=0; i<n; i++){
-               v[i]=start +(2*i );
-                v.push_back(v[i]);
-        }
-        int xor1=0;
-        for(int i=0; i<n; i++){
-            xor1=xor1^v[i];
-        }
-        return xor1;
+              ans^=(start +(2*i ));
+            }   
+        return ans;
     }
 };
