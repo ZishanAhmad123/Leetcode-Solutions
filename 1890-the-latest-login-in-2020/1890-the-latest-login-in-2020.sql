686982e3cc464a674f035f745d709cc027a83def
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-05-28 18:16:04 +0530

    Time: 662 ms (45.47%), Space: 0B (100.00%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+ 
+select user_id, max(time_stamp) as  last_stamp
+from Logins
+  # where time_stamp like '2020%'
+   where year(time_stamp )=2020
+    group by user_id
+  
