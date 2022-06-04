6afc730995bfe0fbf65523253b272ff76f86096a
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-04 08:54:23 +0530

    Time: 21 ms (45.87%), Space: 5.9 MB (74.80%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,9 +2,17 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string s=to_string(x);
-        string y=s;
-       reverse(y.begin(), y.end());
-        if(y==s)return true;
-        else return false;
+       //  string y=s;
+       // reverse(y.begin(), y.end());
+       //  if(y==s)return true;
+       //  else return false;
+        int i=0, j=s.size()-1;
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            else{
+                i++;j--;
+            }
+        }
+        return true;
     }
 };
