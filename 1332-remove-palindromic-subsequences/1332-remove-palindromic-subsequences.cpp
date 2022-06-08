e47a0b891f4dbe22b57b16337a5d719dcefaf0a1
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-08 07:17:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -4,6 +4,8 @@ public:
 {string P = S; reverse(P.begin(), P.end());if (S == P) {return true;}else {return false; }} 
     
     int removePalindromeSub(string s) {
+        if(size(s)==0)return 0;
+        
         if(isPalindrome(s))return 1;
         else return 2;
     }
