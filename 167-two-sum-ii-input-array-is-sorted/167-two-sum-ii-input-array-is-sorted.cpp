5782ffd69281ad35803fecac7a43e646db932456
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-09 07:52:10 +0530

    Time: 11 ms (93.22%), Space: 15.6 MB (75.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-         unordered_map<int, int>mp;
-        for (int i = 0; i < numbers.size(); i++) {
-           int rest = target - numbers[i];
-            if (mp.find(rest) != mp.end()) {
-                return { mp[rest] + 1, i + 1 };
+        //  unordered_map<int, int>mp;
+        // for (int i = 0; i < numbers.size(); i++) {
+        //    int rest = target - numbers[i];
+        //     if (mp.find(rest) != mp.end()) {
+        //         return { mp[rest] + 1, i + 1 };
+        //     }
+        //     mp[numbers[i]] = i;
+        // }
+        // return { 1456,1071};  
+        int i=0, j=numbers.size()-1;
+        while(i<j){
+            int val=numbers[i]+numbers[j];
+            if(target==val){
+                return {i+1, j+1};
             }
-            mp[numbers[i]] = i;
+            else if(val <target){
+                i++;
+                continue;
+            }
+            else{
+                j--;
+                continue;
+            }
         }
-        return { 1456,1071};  
+       return { 1471, 1071}; 
     }
 };
