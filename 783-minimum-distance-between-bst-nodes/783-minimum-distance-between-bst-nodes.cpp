2db2b429a4296dcf861411519a7b95537828b23b
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-10 23:57:04 +0530

    Time: 3 ms (70.39%), Space: 9.9 MB (21.78%) - LeetHub

--- 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
@@ -9,26 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public: 
-    int mini=1e9;
-    int prev=-1 ;
-    void  inorder(TreeNode* root){
-        if(root==NULL)return ;
-        inorder(root->left);
+// class Solution {
+// public: 
+//     int mini=1e9;
+//     int prev=-1 ;
+//     void  inorder(TreeNode* root){
+//         if(root==NULL)return ;
+//         inorder(root->left);
 
-        if(prev==-1)prev=root->val;
-        else
-          mini=min(mini, (root->val-prev));
-        prev=root->val;
+//         if(prev==-1)prev=root->val;
+//         else
+//           mini=min(mini, (root->val-prev));
+//         prev=root->val;
         
-        inorder(root->right);
+//         inorder(root->right);
          
-    }
-    int minDiffInBST(TreeNode* root) {
-        if(root==NULL)return 0;
+//     }
+//     int minDiffInBST(TreeNode* root) {
+//         if(root==NULL)return 0;
         
-        inorder(root);
-        return mini;
-    }
+//         inorder(root);
+//         return mini;
+//     }
+// };
+
+class Solution {
+ public: 
+   void inorder(TreeNode* root, vector<int>&v){
+       if(root==NULL)return ;
+       inorder(root->left, v);
+       v.push_back(root->val);
+       inorder(root->right,v);
+   }
+     int minDiffInBST(TreeNode* root) {
+        if(root==NULL)return 0;
+         vector<int>v;
+        inorder(root, v);
+           int ans=1e9;;
+         for(int i=1; i<size(v); i++){
+             ans=min(ans, (v[i]-v[i-1]));
+         }
+        return ans; 
+     }
 };
