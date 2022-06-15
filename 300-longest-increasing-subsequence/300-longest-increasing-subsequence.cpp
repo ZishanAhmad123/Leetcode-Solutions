63b83fb0d0e78b30dceaf67578ccfd74e20a48fa
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-15 11:28:59 +0530

    Time: 2133 ms (5.02%), Space: 292.4 MB (6.36%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,12 +4,15 @@ public:
         if(nums.size()==i)return 0;
         
         if(dp[i][prev+1]!=-1)return dp[i][prev+1];                //prev+1 in dp because of grid have no -1 index to handle  this we use coordinate shift by +1
+         int maxi=-1e9, not_take=0, take=0;
         
-        int len=0+lnds(nums, i+1, prev,dp);
-        if(prev==-1 || nums[i]>nums[prev]){
-            len=max(len, 1+lnds(nums, i+1, i,dp));
+         not_take=0+lnds(nums, i+1, prev,dp);
+         if(prev==-1 || nums[i]>nums[prev]){
+            take= 1+lnds(nums, i+1, i,dp);
+           
         }
-        return dp[i][prev+1]=len;
+         maxi=max(take, not_take);
+        return dp[i][prev+1]=maxi;
     }
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size(), vector<int>(nums.size()+1, -1));
