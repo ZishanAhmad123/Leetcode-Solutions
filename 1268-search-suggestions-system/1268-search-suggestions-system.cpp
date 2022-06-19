edfeb3d805ede7de440b87a3ec3e5c02095da019
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-19 07:23:43 +0530

    Time: 57 ms (86.24%), Space: 23.4 MB (97.40%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -5,7 +5,7 @@ public:
         int n = searchWord.size();
         vector<vector<string>> res(n);
         for(int i=0; i<products.size(); i++){
-            for(int j=0; j< products[i].size() && j<n; j++){
+            for(int j=0; j< products[i].size(); j++){
                 if(products[i][j]==searchWord[j]){
                     if(res[j].size()<3)res[j].push_back(products[i]);
                 }
