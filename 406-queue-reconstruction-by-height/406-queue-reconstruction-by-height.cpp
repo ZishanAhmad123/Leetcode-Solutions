1ee583ec865e92988091dde51a03f3ab3a40a8ac
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-06-29 09:19:45 +0530

    Time: 246 ms (33.13%), Space: 12 MB (63.86%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  static   bool cmp(vector<int> &a,vector<int> &b)
+{
+    if(a[0]==b[0]) return a[1]<b[1];
+    return a[0]>b[0];
+}
+    
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+         sort(people.begin(),people.end(),cmp);
+          vector<vector<int>> ans;
+        for(int i=0;i<people.size();i++)
+        {
+            if(people[i][1]==ans.size())
+            {
+                ans.push_back(people[i]);
+            }
+            else
+            {
+                ans.insert(ans.begin()+people[i][1],people[i]);
+            }
+        }
+        return ans;
+    }
+};
