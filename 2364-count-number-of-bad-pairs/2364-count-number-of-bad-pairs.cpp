d5a4ea8e21c932c730de5428402759151a319100
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-08-12 16:52:20 +0530

    Time: 448 ms (25.00%), Space: 85.4 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -3,6 +3,13 @@ public:
 #define ll long long
     
     ll countBadPairs(vector<int>& nums) {
+        //for(ll i=0; i<n; i++){
+        //     for(ll j=i+1; j<n; j++){
+        //         if((j-i)!=(nums[j]-nums[i]))cnt++;
+        //     }
+        // }
+        // return cnt;
+        
          map<ll ,ll>mp;
         ll n=nums.size();
         for(ll i=0;i<n;i++){
@@ -10,7 +17,7 @@ public:
             mp[diff]++;
         }
         ll total=(n*(n-1)/2);
-        ll cnt=0;
+         ll cnt=0;
         
         for(auto it=mp.begin();it!=mp.end();it++){      // travers on Map and find value which has value>=1
             if(it->second>1)
@@ -18,5 +25,6 @@ public:
             
         }
         return (total-cnt);
+        
     }
 };
