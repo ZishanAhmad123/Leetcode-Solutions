5e9ab8f4b24e9c4ce986593082d7382ae8b63238
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-08-12 16:42:22 +0530

    Time: 517 ms (25.00%), Space: 85.6 MB (100.00%) - LeetHub

---
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+#define ll long long
+    
+    ll countBadPairs(vector<int>& nums) {
+         map<ll ,ll>mp;
+        ll n=nums.size();
+        for(ll i=0;i<n;i++){
+            ll diff=nums[i]-i;    // count no. of nums[i]-i
+            mp[diff]++;
+        }
+        ll total=(n*(n-1)/2);
+        ll cnt=0;
+        
+        for(auto it=mp.begin();it!=mp.end();it++){      // travers on Map and find value which has value>=1
+            if(it->second>1)
+                cnt+=((it->second)*(it->second-1))/2;
+            
+        }
+        return (total-cnt);
+    }
+};
