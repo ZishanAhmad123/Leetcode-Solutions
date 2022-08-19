1bbda18620274cd0bffd10190ac1e503a428a043
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-08-19 18:26:10 +0530

    Time: 38 ms (28.46%), Space: 27.2 MB (9.73%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,24 +1,59 @@
 class Solution {
 public:
+    
+    int bfs(vector<vector<int>>& grid, int i , int j, vector<vector<bool>> &visited,int count) {
+       // Code here
+       
+       int n = grid.size();
+       int m = grid[0].size();
+       
+      int dx[4]={1,-1,0,0};
+      int dy[4]={0,0,1,-1};
+       
+       queue<pair<int,int>> q;
+       q.push({i,j});
+       visited[i][j] =1;
+       count++;
+       
+       while(!q.empty()){
+           auto f = q.front();
+           int x = f.first;
+           int y = f.second;
+           q.pop();
+           for(int k=0;k<4;k++){
+               int nexti = x+ dx[k];
+               int nextj = y+ dy[k];
+               
+               if(nexti>=0 && nexti<n && nextj>=0 && nextj<m && !visited[nexti][nextj] && grid[nexti][nextj]==1){
+                   count++;
+                   q.push({nexti,nextj});
+                   visited[nexti][nextj] = true;
+                   
+               }
+           }
+       }
+       return count;
+   }
+    
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-      int max_area=0;
-    int row = grid.size();
-    int col = grid[0].size();
-    for(int i=0;i<row;i++)
-        for(int j=0;j<col;j++)
-            if(grid[i][j]==1)
-                max_area = max(max_area , dfs(grid, i, j));
-
-    return max_area;
+      int maxcount = 0;
+            int n = grid.size();
+            int m = grid[0].size();
+       
+          vector<vector<bool>> visited(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+           for(int j=0;j<m;j++){
+           
+                  if(grid[i][j]==1  && !visited[i][j]){
+                   int count = bfs(grid,i,j,visited,0); //count gets updated
+                   maxcount = max(maxcount ,count );
+                  }
+           }
+       }
+       return maxcount ;
 }
-int dfs(vector<vector<int>>& grid, int i, int j)
-{
-     if( i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1){
-        grid[i][j] = 0;
-        return 1 + dfs(grid, i+1, j) + dfs(grid, i-1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
-    }
-    return 0;
-        
+
      
 //         int Max=0;
 //          int row = grid.size();
@@ -43,6 +78,5 @@ int dfs(vector<vector<int>>& grid, int i, int j)
 //          if(dfs(grid, i,j-1, cnt))cnt++;
 //          if(dfs(grid, i,j+1, cnt))cnt++;
 //         return 1;
-    }
     
 };
