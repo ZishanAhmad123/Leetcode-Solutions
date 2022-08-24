66322964c957613b013ce44245ca21044fc3e3c8
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-08-24 11:37:01 +0530

    Time: 20 ms (88.73%), Space: 6.1 MB (12.98%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,15 @@
+
+#include "bits/stdc++.h"
+using namespace std;
+class Solution {
+public:
+   
+    bool isPowerOfThree(int n) {
+      for(int i=0;pow(3,i)<=n;i++){
+            if(pow(3,i)==n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
