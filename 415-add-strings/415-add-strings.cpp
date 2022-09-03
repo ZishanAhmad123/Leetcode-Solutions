87ad74410e05156a177de1a0d90e5402f9686724
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-09-03 08:51:57 +0530

    Time: 3 ms (88.27%), Space: 6.8 MB (37.95%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string addStrings(string X, string Y) {
+         int carry=0;
+        int nx=X.size(), ny=Y.size();
+        string sum="";
+        int i=nx-1,j=ny-1,s;
+
+        for(i;i>=0 && j>=0;i--,j--)
+        {
+            s=(X[i]-'0'+Y[j]-'0'+carry);
+            sum += ((s%10)+'0');
+            carry=s/10;
+        }
+           
+        while(i>=0)
+        {
+            s=(X[i]-'0'+carry);
+            sum += ((s%10)+'0');
+            carry=s/10;
+            i--;
+        }
+        
+        while(j>=0)
+        {
+            s=(Y[j]-'0'+carry);
+            sum += ((s%10)+'0');
+            carry=s/10;
+            j--;
+        }
+        
+        if(carry>0)
+        {
+            sum+=(carry+'0');
+        }
+        while(sum.back()=='0')
+            sum.pop_back();
+            
+        reverse(sum.begin(),sum.end());
+        
+        if(sum.size()==0) sum="0";
+        return sum;
+    }
+};
