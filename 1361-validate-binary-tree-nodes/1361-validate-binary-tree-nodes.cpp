93949841b253cccecd49e03c6880da0fa9a619eb
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-09-25 15:32:07 +0530

    Time: 168 ms (21.62%), Space: 44.9 MB (19.84%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
+     // topological sort
+    vector<int> topoSort(int V, vector<vector<int>>&adj) {
+    vector <int> indegree(V, 0);
+     vector <int> topo;
+	    for(int i=0; i<V; i++){
+	        for(auto j:adj[i])
+	            indegree[j]++;
+	    } queue <int> q;
+	    for(int i=0; i<V; i++){
+             if(indegree[i]>1)return { } ;
+	        if(indegree[i] == 0)  q.push(i); }
+	    int cnt=0;
+	    while(!q.empty()){
+	        int t = q.front();  q.pop(); cnt++;
+	        topo.push_back(t);
+	        for(auto i:adj[t]){
+	            indegree[i]--;
+	            if(indegree[i] == 0)q.push(i);}  }
+	   if(cnt==V)return topo;                  
+         else return {};}
+    
     bool validateBinaryTreeNodes(int n, vector<int>& left, vector<int>& right) {
       
-        vector<vector<int>>mat(n) ;
+        vector<vector<int>>mat(n) ;        // making  possible graph 
         int edges = 0 ;
-        vector<int>visited(n,0) ;
         vector<int>indeg(n,0) ;
         
         for(int i=0;i<n;i++)
@@ -13,50 +33,19 @@ public:
             {
                 mat[i].push_back(left[i]) ;
                 edges++ ;
-                indeg[left[i]]++ ;
             }
         }
-        
         for(int i=0;i<n;i++)
         {
             if( right[i] != -1 )
             {
                 mat[i].push_back(right[i]) ;
                 edges++ ;
-                indeg[right[i]]++ ;
             }
         }
         
-        queue<int>pq;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(indeg[i]>1)return false ;
-            
-            if(indeg[i]==0)
-                pq.push(i) ;
+      vector<int> topo=topoSort(n, mat);        //if graph have no toposort means no cylce yani that graph is tree                                                                 count(edges) should be n-1 for tree
+        if(topo.size()!=n || edges!=n-1)return false;
+        return true;
         }
-        
-        int cnt = 0 ;
-        
-        while(!pq.empty())
-        {
-            auto temp = pq.front() ;
-            pq.pop() ;
-            
-           // visited[temp] = 1 ;
-            cnt++ ;
-            
-             for(auto i:mat[temp]){
-	            indeg[i]--;
-	            if(indeg[i] == 0)pq.push(i);} 
-            
-            
-        }
-        
-        if( cnt != n || edges != n-1 )
-            return false ;
-        
-        return true ;
-    }
 };
