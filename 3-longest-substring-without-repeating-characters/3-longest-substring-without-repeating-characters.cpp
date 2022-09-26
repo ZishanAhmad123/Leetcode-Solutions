12bc8ac1abb8171f9e6e0d58e225475b30647132
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-09-26 20:06:29 +0530

    Time: 42 ms (37.63%), Space: 8.9 MB (41.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,55 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
        
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        set<int>st;
-        while (left < s.length()) {
-            auto it =st.find(s[left]);
-            if(it==st.end()){
-                if(left-right+1>ans)
-                    ans=left-right+1;
-                    st.insert(s[left]);
-                left++;
-            }
-            else{
-                st.erase(s[right]);
-                right++;
-            }
+//         int left = 0;
+//         int right = 0;
+//         int ans = 0;
+//         set<int>st;
+//         while (left < s.length()) {
+//             auto it =st.find(s[left]);
+//             if(it==st.end()){
+//                 if(left-right+1>ans)
+//                     ans=left-right+1;
+//                     st.insert(s[left]);
+//                 left++;
+//             }
+//             else{
+//                 st.erase(s[right]);
+//                 right++;
+//             }
             
-        }
+//         }
        
-        return ans;
+//         return ans;
+//     }
+// };
+class Solution {
+public:        
+    int lengthOfLongestSubstring(string str) {        
+         unordered_map<char, int> mp;
+  int max_len = 0;
+  int left = 0, right = 0;
+  int k;
+  while (right < str.size()) {
+    ++mp[str[right]];
+    k = (right - left + 1);
+    if (mp.size() == k) {
+      max_len = max(max_len, k);
+    } else {
+      while (mp.size() < k) {
+        char ch = str[left];
+        --mp[ch];
+        if (mp[ch] == 0) mp.erase(ch);
+        left++;
+        k--;    // increasing left meaning window size is shrinked.
+      }
+      if (mp.size() == k) {
+        max_len = max(max_len, k);
+      }
+    }
+    right++;
+  }
+  return max_len;
     }
 };
