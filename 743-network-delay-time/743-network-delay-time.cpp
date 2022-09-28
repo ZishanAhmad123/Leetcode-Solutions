791e06b40d7ba75c4e3d7db43670a5c00da75289
Zishan Ahmad <87009378+ZishanAhmad123@users.noreply.github.com>
2022-09-28 07:59:21 +0530

    Time: 294 ms (20.99%), Space: 43.5 MB (37.25%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> Dijkstra(int& n,  vector<vector<pair<int, int>>>&graph, int &V) {
+      vector<int> distanceToLastNode(V+1, 1e9);
+    distanceToLastNode[n] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>,greater<pair<int, int>>> pq;
+    pq.push({0, n});
+      while (!pq.empty()) {
+           int u=pq.top().second;
+            int distance=pq.top().first;
+            pq.pop();
+            /* TLE Elimination */
+            // if(distance>distanceToLastNode[u])
+            //     continue;
+      for (auto next : graph[u]) {
+        int v = next.first, dis = next.second;
+        if (distanceToLastNode[v] > distanceToLastNode[u] + dis) {
+          distanceToLastNode[v] = distanceToLastNode[u] +dis;
+          pq.push({distanceToLastNode[v], v});
+        }
+      }
+    }
+        return distanceToLastNode;
+  }
+    
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+           
+        vector<int>dis(n+1,1e9);
+        vector<vector<pair<int, int>>>adj(n+1);
+        
+        for(auto t: times){
+            adj[t[0]].push_back({t[1],t[2]});
+        }
+        dis=Dijkstra(k, adj, n);
+        
+        int ans=0;
+        for(int i=1;i<=n;i++)
+           {
+            if(dis[i]==1e9) return -1;    // If any node is not reachable from k
+            ans=max(ans,dis[i]);
+           }
+        return ans;
+        
+    }
+};
